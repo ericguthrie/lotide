3d10cdc95fb57e3b73ce4f58e12baa3843252b9e
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-01 15:48:28 -0800

    Add files via upload

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  :)`)
+  }
+  else {
+    console.log(`Assertion Failed: :(`)
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("ERIC", "ERIC");
+assertEqual(1,2);
