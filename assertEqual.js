dee43ab7ba38fc0fa5739ead9c105628b4f51303
ericguthrie <ericguthrie12@gmail.com>
2021-03-01 23:21:41 +0000

    assert  practice

--- assertEqual.js
+++ assertEqual.js
@@ -10,4 +10,4 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual("ERIC", "ERIC");
-assertEqual(1,2);
+assertEqual(1,2);a
