75aa705f541d22679dac7d4e57ab04d3ca531ff0
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-01 17:16:31 -0800

    Add files via upload

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  :)`)
+  }
+  else {
+    console.log(`Assertion Failed: :(`)
+  }
+};
+
+const tail = function(words) {
+  return words[1]
+}
+
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
