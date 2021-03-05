48b4cf3870ea824976de318a9b3cb4f7a804d4a2
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-04 20:20:21 -0800

    Add files via upload

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed:  :)`)
+  }
+  else {
+    console.log(`Assertion Failed: :(`)
+  }
+};
+
+function findKey(object, value) {
+  let output = undefined; 
+  for(let key in object) {
+    if(value(object[key])) {   
+      output = key;
+      return output
+    }
+  }
+  return output
+}
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)) // => "noma"
