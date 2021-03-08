63cdcc73621eb58a7e8b9357418448d1707a35dc
ericguthrie <ericguthrie12@gmail.com>
2021-03-08 20:17:39 +0000

    added assert equal via require

---
+++ tail.js
@@ -0,0 +1,12 @@
+const assertEqual = require('./assertEqual');
+
+const tail = function(words) {
+  newArray = words;
+  newArray.slice(1);
+}
+
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
