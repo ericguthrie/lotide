d3ae6019e7e5ead7164132aba4e0c9c781f18724
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-08 13:39:35 -0800

    Add files via upload

--- head.js
+++ head.js
@@ -10,10 +10,10 @@ const assertEqual = require('./assertEqual');
 
 const head = function(array) {
   newArray = array;
-  return newArray.shift(0)
+  return newArray.shift(0);
 }
 
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
+
 
