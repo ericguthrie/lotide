0ba8b4bbd80aa70a10a9fe87d274296eb4d83da4
ericguthrie <ericguthrie12@gmail.com>
2021-03-08 19:59:20 +0000

    adding tests to function

--- assertEqual.js
+++ assertEqual.js
@@ -1,13 +1,11 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: :)")
+    console.log(`Assertion Passed:  :)`)
   }
   else {
-    console.log("Assertion Failed: :(")
+    console.log(`Assertion Failed: :(`)
   }
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("ERIC", "ERIC");
-assertEqual(1,2);a
+module.exports = assertEqual;
+
