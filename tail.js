a551144a2b64fdd69ab32fd7fd73a82e2e3d9c3f
ericguthrie <ericguthrie12@gmail.com>
2021-03-08 20:24:24 +0000

    adding test functions

--- tail.js
+++ tail.js
@@ -5,8 +5,4 @@ const tail = function(words) {
   newArray.slice(1);
 }
 
-
-// Test Case: Check the original array 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
