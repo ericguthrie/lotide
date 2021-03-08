94029681aef352a26f61349c04d998f375d10676
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-08 13:22:20 -0800

    Add files via upload

--- middle.js
+++ middle.js
@@ -1,23 +1,4 @@
-const eqArrays = (a, b) => {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false; 
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function(a, b) {
-  if (eqArrays(a, b)) {
-
-      console.log(`Assertion Passed: :) ${a} === ${b}`);
-    } else {
-    console.log(`Assertion Failed: :( ${a} !== ${b}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 
 const middle = nums => { 
@@ -33,5 +14,5 @@ const middle = nums => {
   }
 }
 
-console.log(middle([1, 2, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
+
---
+++ middleTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('./assertArraysEqual');
+const middle = require('./middle');
+
+console.log(middle([1, 2, 4])) // => [2, 3]
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
