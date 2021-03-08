9a47f3c233453ffd18d942ee13155a6ea09a08de
ericguthrie <ericguthrie12@gmail.com>
2021-03-08 21:08:57 +0000

    adding test conditions

---
+++ assertArraysEqual.js
@@ -0,0 +1,12 @@
+const eqArrays = require('./eqArrays');
+
+const assertArraysEqual = function(a, b) {
+  if (eqArrays(a, b)) {
+
+      console.log(`Assertion Passed: :) ${a} === ${b}`);
+    } else {
+    console.log(`Assertion Failed: :( ${a} !== ${b}`);
+  }
+};
+
+module.exports = assertArraysEqual;
