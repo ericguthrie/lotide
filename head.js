b78889df5532563cab3f4f5bc701bc910a92e701
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-08 12:07:40 -0800

    Add files via upload

--- head.js
+++ head.js
@@ -1,14 +1,16 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed:  :)`)
-  }
-  else {
-    console.log(`Assertion Failed: :(`)
-  }
-};
+// const assertEqual = function(actual,expected) {
+//   if (actual === expected) {
+//     console.log(`Assertion Passed:  :)`);
+//   }
+//   else {
+//     console.log(`Assertion Failed: :(`);
+//   }
+// };
+const assertEqual = require('./assertEqual');
 
 const head = function(array) {
-  return array[0]
+  newArray = array;
+  return newArray.shift(0)
 }
 
 
