c86290316341931d647a930c2ed5b8b9acab49e8
Eric Guthrie <77245535+ericguthrie@users.noreply.github.com>
2021-03-08 13:13:14 -0800

    Add files via upload

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = (a, b) => {
-  if (a.length !== b.length) {
-    return false;
-  }
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] !== b[i]) {
-      return false; 
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(a, b) {
   if (eqArrays(a, b)) {
@@ -18,3 +8,5 @@ const assertArraysEqual = function(a, b) {
     console.log(`Assertion Failed: :( ${a} !== ${b}`);
   }
 };
+
+module.exports = assertArraysEqual;
